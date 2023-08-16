3c1cd266cf369b63cb040b7dc9d753fd0245e90a
pinchao <cpc4253@163.com>
2023-08-16 21:39:18 +0800

    finish bundler by generateCode

--- bundler.js
+++ bundler.js
@@ -3,7 +3,7 @@
 // 3. 拿到依赖关系，用 @babel/traverse 从 ast 中找到 import 的节点，拿到所有的依赖关系（{filename, dependencies}）
 // 4. 转换成浏览器可运行代码，用 @babel/core 解析 esm 模块代码为浏览器可运行代码 ({filename, dependencies, code})
 // 5. 递归生成整个项目的依赖图谱
-// 6.
+// 6. 生成最终代码，依赖图谱中的 require 以及 exports 在浏览器不存在，需要构造
 
 const path = require('path');
 const fs = require('fs');
@@ -66,5 +66,30 @@ const makeDependenciesGraph = (entry) => {
   return graph;
 };
 
-const graphInfo = makeDependenciesGraph('./src/index.js');
-console.log(graphInfo);
+// 生成最终代码（主要是 requrie 方法的构建和 exports 对象的处理）
+const generateCode = (entry) => {
+  // 将 js 对象转换为字符串
+  const graph = JSON.stringify(makeDependenciesGraph(entry));
+  // 经过 @babel/core 代码转换，import 语法被处理成 require 方法；export 语法被处理成 exports 对象
+  // 传入依赖图谱字符串，如果 eval 出的代码里有 require，那么就执行传入 localRequire 方法
+  // localRequire 方法：根据 dependencies[relativePath]，继续递归执行外部作用域的 require 方法，又会再次进入 eval，执行代码
+  // exports 对象处理：当执行 require 方法时，如果模块里的通过 exports 导出属性或者方法时，需要有个空对象承接，然后下一个模块才能用这个结果。
+  return `
+    (function (graph) {
+      function require(module) {
+        function localRequire(relativePath) {
+          return require(graph[module].dependencies[relativePath]);
+        }
+        var exports = {};
+        (function (require, exports, code) {
+          eval(code);
+        })(localRequire, exports, graph[module].code);
+        return exports;
+      }
+      require('${entry}');
+    })(${graph})
+  `;
+};
+
+const code = generateCode('./src/index.js');
+console.log(code);
