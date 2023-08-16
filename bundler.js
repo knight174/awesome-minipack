a87ff4e7d0a282ccfad8e6518899a47e335fba7a
pinchao <cpc4253@163.com>
2023-08-16 19:31:18 +0800

    bundler graph

--- bundler.js
+++ bundler.js
@@ -2,6 +2,8 @@
 // 2. 拿到文件依赖，用 @babel/parser 解析代码，拿到抽象语法树 ast（ast.program.body）
 // 3. 拿到依赖关系，用 @babel/traverse 从 ast 中找到 import 的节点，拿到所有的依赖关系（{filename, dependencies}）
 // 4. 转换成浏览器可运行代码，用 @babel/core 解析 esm 模块代码为浏览器可运行代码 ({filename, dependencies, code})
+// 5. 递归生成整个项目的依赖图谱
+// 6.
 
 const path = require('path');
 const fs = require('fs');
@@ -9,6 +11,7 @@ const parser = require('@babel/parser');
 const traverse = require('@babel/traverse').default;
 const babel = require('@babel/core');
 
+// 模块依赖
 const moduleAnalyser = (filename) => {
   const content = fs.readFileSync(filename, 'utf-8');
   const ast = parser.parse(content, {
@@ -38,5 +41,30 @@ const moduleAnalyser = (filename) => {
   };
 };
 
-const moduleInfo = moduleAnalyser('./src/index.js');
-console.log(moduleInfo);
+// 依赖图谱
+const makeDependenciesGraph = (entry) => {
+  const entryModule = moduleAnalyser(entry);
+  const graphArray = [entryModule];
+  for (let i = 0; i < graphArray.length; i++) {
+    const item = graphArray[i];
+    const { dependencies } = item;
+    // 递归：如果该模块还有依赖，那么继续放进 moduleAnalyser 拿到下一个 module，然后 push 到 graphArray，最后拿到最终到依赖图
+    if (dependencies) {
+      for (const dep in dependencies) {
+        graphArray.push(moduleAnalyser(dependencies[dep]));
+      }
+    }
+  }
+  // 数据结构转换
+  const graph = {};
+  graphArray.forEach((item) => {
+    graph[item.filename] = {
+      dependencies: item.dependencies,
+      code: item.code,
+    };
+  });
+  return graph;
+};
+
+const graphInfo = makeDependenciesGraph('./src/index.js');
+console.log(graphInfo);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import message from './message.js';
-import message2 from './message2.js';
+// import message2 from './message2.js';
 
 console.log(message);
